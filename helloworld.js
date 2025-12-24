54670ac01459c59ff99f7afae5796eea2be18261
ChTharunKumar <118474101+ChTharunKumar@users.noreply.github.com>
2025-12-24 11:33:04 +0530

    Create helloworld.js

---
+++ helloworld.js
@@ -0,0 +1,5 @@
+// Simple console output
+console.log("-----------------------------------------");
+console.log("Hello, World! Node.js is running successfully.");
+console.log("Current Time: " + new Date().toLocaleString());
+console.log("-----------------------------------------");
